955d4cbbe7ac0538a9dcbf1f58fe03ede2e78df6
Olga <olgajensen@mail.ru>
2023-01-25 19:30:09 +0300

    operations

---
+++ mod_calc.py
@@ -0,0 +1,48 @@
+import logg
+
+def sum(a, b):
+    res = a + b
+    logg.logging.info(f'Sum: {a} + {b} = {res}')
+    return res
+    
+def sub(a, b):
+    res = a - b
+    logg.logging.info(f'Sub: {a} - {b} = {res}')
+    return res
+    
+def mult(a, b):
+    res = a * b
+    logg.logging.info(f'Mult: {a} * {b} = {res}')
+    return res
+    
+def div1 (a, b):
+    if b == 0:
+        return 'Division is impossible'
+    res = a / b
+    logg.logging.info(f'Div: {a} / {b} = {res}')
+    return res
+    
+def int_div2(a, b):
+    if b == 0:
+        return 'Division is impossible'
+    res = a // b
+    logg.logging.info(f'Div: {a} // {b} = {res}')
+    return res
+    
+def div3(a, b):
+    if b == 0:
+        return 'Division is impossible'
+    res = a % b
+    logg.logging.info(f'Div: {a} % {b} = {res}')
+    return res
+    
+def pow(a, b):
+    res = a ** b
+    logg.logging.info(f'Pow: {a} ** {b} = {res}')
+    return res
+    
+def sqrt(a):
+    res = a ** 0.5
+    logg.logging.info(f'Sqrt: {a} ** 0.5 = {res}')
+    return res
+    
